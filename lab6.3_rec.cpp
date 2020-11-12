3b047d330f52fd1192437629c0a58ef9ac43de38
Ivan Patiuk <gendalf2003@gmail.com>
2020-11-12 13:59:44 +0200

    lab6.3_rec_fix

--- lab6.3_rec.cpp
+++ lab6.3_rec.cpp
@@ -4,38 +4,11 @@
 
 using namespace std;
 
+void create(int* a, int Low, int Max, int N, int i);
+void print(int* a, int N, int i);
 template <typename T>
-T count1(T* a, T N, T i, T j)
-{
-	if (i <= N - 1)
-	{
-		if (a[i] % 2 == 0)
-		{
-			++j;
-		}
-		return count1(a, N, ++i, j);
-	}
-	else
-		return j;
-}
-void print(int* a, int N, int i)
-{
-	if (i <= N - 1)
-	{
-		cout << setw(3) << a[i] << " ";
-		return print(a, N, ++i);
-	}
-}
-void create(int* a, int Low, int Max, int N, int i)
-{
-	if (i <= N - 1)
-	{
-		a[i] = Low + rand() % (Max - Low + 1);
-		return create(a, Low, Max, N, ++i);
-	}
-	else
-		print(a, N, 0);
-}
+T count1(T* a, T N, T i, T j);
+
 int main()
 {
 	int N, Low, Max;
@@ -48,7 +21,38 @@ int main()
 	cout << "Введіть максимальне значення елементу масиву: "; cin >> Max;
 	int* a = new int[N];
 	create(a, Low, Max, N, 0);
+	print(a, N, 0);
 	cout << endl;
 	cout << "Кількіть парних: " << count1(a, N, 0, 0) << endl;
 	delete[] a;
+}
+void create(int* a, int Low, int Max, int N, int i)
+{
+	if (i <= N - 1)
+	{
+		a[i] = Low + rand() % (Max - Low + 1);
+		create(a, Low, Max, N, ++i);
+	}
+}
+void print(int* a, int N, int i)
+{
+	if (i <= N - 1)
+	{
+		cout << setw(3) << a[i] << " ";
+		print(a, N, ++i);
+	}
+}
+template <typename T>
+T count1(T* a, T N, T i, T j)
+{
+	if (i <= N - 1)
+	{
+		if (a[i] % 2 == 0)
+		{
+			++j;
+		}
+		count1(a, N, ++i, j);
+	}
+	else
+		return j;
 }
